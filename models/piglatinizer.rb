66c6cc74e795c24874ad997d3177979b2fc24c8d
Paul K <pkfis@outlook.com>
2020-01-28 17:26:19 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinizer
+
+  def piglatinize(text)
+    words = text.split(" ")
+    words.collect! do |word|
+      word_a = word.split("")
+      if !word_a[0].downcase.match?(/[aeoui]/)
+        until word_a[0].downcase.match?(/[aeiou]/)
+          word_a << word_a.shift
+        end
+        word_a.join << "ay"
+      else
+        word << "way"
+      end
+    end
+    words.join(" ")
+  end
+  
+end
